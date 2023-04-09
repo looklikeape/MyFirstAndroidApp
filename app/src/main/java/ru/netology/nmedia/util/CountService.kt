cb2e5a13829f2e37ee961c5e3ceab79e4df8bff9
ruslan <ruslan.pohod@gmail.com>
2023-04-09 13:22:27 +0300

    MVVM realization v2: minSDK change and idea folder deleted

--- app/src/main/java/ru/netology/nmedia/util/CountService.kt
+++ app/src/main/java/ru/netology/nmedia/util/CountService.kt
@@ -1,26 +1,19 @@
 package ru.netology.nmedia.util
 
-import ru.netology.nmedia.dto.Post
 import kotlin.math.floor
 
-fun postLikeCounter (post:Post):String {
-        val pl = when (post.likes) {
-            in 0..999 -> post.likes.toString()
-            in 1000..999999 -> (floor(post.likes.toDouble()/100)/10).toString() + "K"
-            else -> {
-                (floor(post.likes.toDouble() / 100000)/10).toString() + "M"
-            }
-        }
-        return pl
+
+fun postLikeShareCounter(x: Int): String {
+    val count = when (x) {
+        in 0..999 -> x.toString()
+        in 1000..999999 -> if (x % 1000 < 100) {
+            (x / 1000).toString() + "K"
+        } else (floor(x.toDouble() / 100) / 10).toString() + "K"
+        else -> if (x % 1_000_000 < 100_000) {
+            (x / 1_000_000).toString() + "M"
+        } else (floor(x.toDouble() / 100000) / 10).toString() + "M"
     }
-fun postShareCounter (post:Post):String{
-    val ps = when(post.shares) {
-        in 0..999 -> post.shares.toString()
-        in 1000..999999 -> (floor(post.shares.toDouble()/100)/10).toString() + "K"
-        else -> {
-            (floor(post.shares.toDouble() / 100000)/10).toString() + "M"
-        }
-    }
-        return ps
-    }
+    return count
+}
+
 
